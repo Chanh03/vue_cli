8614106c553a38acdb566ad64e2aa2ad978df7fb
Anh Anh <ngovietanh121@gmail.com>
2024-12-20 22:28:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <app-header ref="header" />
+  <app-modal
+    title="Chào chuột ngoooooooo"
+    content="Còn cái này hay lắm"
+    theme="d"
+  />
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppModal from "./components/AppModal.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppModal,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="modal fade show">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1>{{ title }}</h1>
+        </div>
+        <div class="modal-body" :class="{ 'bg-purple': theme == 'purple' }">
+          <img
+            src="https://reviewamthuc.net/wp-content/uploads/2024/04/chuot-nuong-4.jpg"
+            alt=""
+          />
+        </div>
+        <div class="modal-footer">
+          <h3>
+            <button>LIKE</button>
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    footer: {
+      type: String,
+      default: "",
+    },
+    theme: {
+      type: String,
+      default: "white",
+      validator(value) {
+        return ["purple", "green", "red", "yellow", "white"].includes(value);
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.bg-purple {
+  background: purple;
+}
+.modal {
+  position: fixed;
+  top: 100px;
+  left: 0;
+  z-index: var(--bs-modal-zindex);
+  display: block;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+}
+.fade {
+  transition: opacity 0.15s linear;
+}
+</style>
